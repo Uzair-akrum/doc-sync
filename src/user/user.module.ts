8756780a97a2cfa9af6718d2fc3dcd1202e7b939
Uzair-Akrum <limiaraion@gmail.com>
2023-12-14 23:32:45 +0500

    Subscrbing to events fucntionality on follow user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { KafkaModule } from 'src/kafka/kafka.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, KafkaModule],
   providers: [UserResolver, UserService],
 })
 export class UserModule {}
