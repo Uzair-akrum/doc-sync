53c679c4c31eafa3f9f4182417b1f3d6f7e73579
Uzair-Akrum <limiaraion@gmail.com>
2023-12-14 15:14:55 +0500

    Added kafka admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,20 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { UserModule } from './user/user.module';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { KafkaModule } from './kafka/kafka.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     UserModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
+    KafkaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    KafkaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
