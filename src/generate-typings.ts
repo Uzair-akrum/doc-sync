552a3bab71b4d4b3e651b9e9803d1ae68e5748fa
Uzair-Akrum <limiaraion@gmail.com>
2023-12-18 16:01:14 +0500

    reverting generate typings

--- src/generate-typings.ts
+++ src/generate-typings.ts
@@ -1,13 +1,10 @@
-// src/generate-typings.ts
-
-import { GraphQLSchemaHost } from '@nestjs/graphql';
 import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
 import { join } from 'path';
-import { writeFile } from 'fs/promises';
 
-async function generateTypings() {
-  const schemaHost = new GraphQLSchemaHost();
-  const graphqlDefinitionsFactory = new GraphQLDefinitionsFactory();
-}
-
-generateTypings();
+const definitionsFactory = new GraphQLDefinitionsFactory();
+definitionsFactory.generate({
+  typePaths: ['./src/**/*.graphql'],
+  path: join(process.cwd(), 'src/graphql.ts'),
+  outputAs: 'class',
+  watch: true,
+});
