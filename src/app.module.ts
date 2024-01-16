af8978a997b05b7034eff70c26a9822bc45e82f1
Uzair-Akrum <limiaraion@gmail.com>
2024-01-16 14:31:22 +0500

    chore : cors addition

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 import { UserModule } from './user/user.module';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { KafkaModule } from './kafka/kafka.module';
 import { ConfigModule } from '@nestjs/config';
-import { PostModule } from './post/post.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+ import { ScheduleModule } from '@nestjs/schedule';
+import { DocumentModule } from './document/document.module';
 @Module({
   imports: [
+    DocumentModule,
     UserModule,
-    PostModule,
+    ScheduleModule.forRoot(),
+    EventEmitterModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
-    KafkaModule,
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    }),
-    KafkaModule,
-    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService],
